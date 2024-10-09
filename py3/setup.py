797dc07acaf702530cc0b99eeb588313dd78335f
Mohsen Banan <form@mohsen.banan.1.byname.net>
2024-10-08 18:40:24 -0700

    Modernized.

--- py3/setup.py
+++ py3/setup.py
@@ -1,30 +1,54 @@
 #!/usr/bin/env python
 
+# Template File:  "/bisos/apps/defaults/begin/templates/purposed/pyModule/python/setup.py"
+
+####+BEGIN: bx:dblock:global:file-insert :mode python :file "/bisos/apps/defaults/begin/templates/purposed/pyModule/python/commonSetupCode.py"
+
 import setuptools
 import re
+import inspect
+import pathlib
 
+def pkgName():
+    """ From this eg., filepath=.../bisos-pip/PkgName/py3/setup.py, extract PkgName. """
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    grandMother = pathlib.Path(filename).resolve().parent.parent.name
+    return f"bisos.{grandMother}"
 
-def readme():
-    with open('./README.org') as file:
-        while line := file.readline():
-            if match := re.search(r'^#\+title: (.*)',  line.rstrip()):
-                return match.group(1)
-            return "MISSING TITLE in ./README.org"
+def description():
+    """ Extract title from ./README.org which is expected to have a title: line. """
+    try:
+        with open('./README.org') as file:
+            while line := file.readline():
+                if match := re.search(r'^#\+title: (.*)',  line.rstrip()):
+                    return match.group(1)
+                return "MISSING TITLE in ./README.org"
+    except IOError:
+        return  "ERROR: Could not read ./README.org file."
 
 def longDescription():
+    """ Convert README.org to README.rst. """
     try:
         import pypandoc
     except ImportError:
-        result = "warning: pypandoc module not found, could not convert to RST"
+        result = "WARNING: pypandoc module not found, could not convert to RST"
         return result
-    return pypandoc.convert_file('README.org', 'rst')
+    if (result := pypandoc.convert_file('README.org', 'rst')) is None:
+        result = "ERROR: pypandoc.convert_file('README.org', 'rst') Failed."
+    return result
 
-####+BEGINNOT: b:py3:pypi/nextVersion :increment 0.01
-
-def pkgVersion(): return '0.5'
 ####+END:
 
-####+BEGIN: b:py3:pypi/requires :extras ("pygit2")
+# :curDevVer "0.92" :pypiNextVer "0.95"
+####+BEGIN: b:py3:pypi/nextVersion :increment 0.01
+
+# ./pypiUploadVer does not exist -- pypiNextVer=0.41 -- installedVersion=0.5
+def pkgVersion():
+        return '0.5'  # Version Nu Of Installed Pkg
+
+####+END:
+
+####+BEGIN: b:py3:pypi/requires :extras ()
 
 requires = [ 
 "blee",
@@ -38,25 +62,33 @@ requires = [
 "bisos.icm",
 "bisos.pals",
 "from",
-"pygit2",
 ]
 ####+END:
 
 ####+BEGIN: b:py3:pypi/scripts :comment ""
 
-scripts = [
+scripts = [ 
 './bin/bpoManage.cs',
+'./bin/bpoRepos.cs',
 ]
 ####+END:
 
+#
+# Data files would be  specified in ./MANIFEST.in as: # recursive-include bisos/pkgName *
+#
+
+data_files = [
+]
+
+# :pkgName "--auto--"  --- results in use of name=pkgName(),
+####+BEGIN: b:py3:pypi/setupFuncArgs :pkgName ""
 
 setuptools.setup(
     name='bisos.bpo',
     version=pkgVersion(),
-    # namespace_packages=['bisos'],
     packages=setuptools.find_packages(),
     scripts=scripts,
-    requires=requires,
+    #data_files=data_files,
     include_package_data=True,
     zip_safe=False,
     author='Mohsen Banan',
@@ -65,7 +97,7 @@ setuptools.setup(
     maintainer_email='libre@mohsen.1.banan.byname.net',
     url='http://www.by-star.net/PLPC/180047',
     license='AGPL',
-    description=readme(),
+    description=description(),
     long_description=longDescription(),
     download_url='http://www.by-star.net/PLPC/180047',
     install_requires=requires,
@@ -79,3 +111,5 @@ setuptools.setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         ]
     )
+
+####+END:
